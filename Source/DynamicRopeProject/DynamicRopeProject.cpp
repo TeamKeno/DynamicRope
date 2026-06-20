4d7eb3e2834e51f41391fc0bc531fabaaf25a384
Shimwoojin <ggoggal@gmail.com>
2026-06-20 11:16:04 +0900

    Initial project setup
    Perforce-Change: 3

---
+++ Source/DynamicRopeProject/DynamicRopeProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DynamicRopeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DynamicRopeProject, "DynamicRopeProject" );
